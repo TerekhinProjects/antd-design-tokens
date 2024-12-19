ceee7d9b319a2631a4ed233aee94b3f69a13e954
Konstantin Teryohin <konstantin.teryohin@rubius.com>
2024-12-19 11:58:08 +0700

    feat: example of override and extend styles of components

--- src/app/theme/theme.provider.tsx
+++ src/app/theme/theme.provider.tsx
@@ -1,9 +1,12 @@
-import { themeConfig } from './theme.config.ts';
-import theme from './theme.json';
-import { ConfigProvider, ThemeConfig } from 'antd';
 import { PropsWithChildren } from 'react';
+import { ConfigProvider, ThemeConfig } from 'antd';
+import { themeConfig } from './theme.config.ts';
+import { useStyle } from './theme.override.ts';
+import theme from './theme.json';
 
 export const ThemeProvider = (props: PropsWithChildren) => {
+    const { styles } = useStyle();
+
     return (
         <ConfigProvider
             {...themeConfig}
@@ -11,6 +14,8 @@ export const ThemeProvider = (props: PropsWithChildren) => {
                 ...theme as unknown as ThemeConfig,
                 cssVar: { key: 'app', prefix: '' }
             }}
+            button={{ className: styles.linearGradientButton }}
+            card={{ className: styles.card }}
             children={props.children}
         />
     );
